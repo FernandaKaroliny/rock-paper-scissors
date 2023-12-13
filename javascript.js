470b6c2a620d2aa4df1793e1faff176a15e2ed54
FernandaKaroliny <fernandaks2020@outlook.com>
2023-12-13 08:14:55 -0300

    Adiciona contagem de pontos

--- javascript.js
+++ javascript.js
@@ -6,45 +6,60 @@ function getComputerChoice() {
 
 let playerSelection;
 let computerSelection;
+let score = 0;
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === "rock") {
+function playRound() {
+    if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
+            score++;
             return ("You win! Rock beats scissors");
-        } else if (computerSelection === "paper"){
+        } else if (computerSelection === "paper") {
             return ("You Lose! Paper beats Rock");
         } else if (computerSelection === "rock") {
             return ("It's a tie! Play again");
         }
-    }else if(playerSelection === "scissors"){
+    } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             return ("You Lose! Rock beats scissors");
-        } else if (computerSelection === "paper"){
+        } else if (computerSelection === "paper") {
+            score++;
             return ("You win! Scissors beats paper");
         } else if (computerSelection === "scissors") {
             return ("It's a tie! Play again");
         }
-    }else if(playerSelection === "paper"){
+    } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+            score++;
             return ("You win! Paper beats Rock");
-        }else if (computerSelection === "scissors") {
+        } else if (computerSelection === "scissors") {
             return ("You Lose! Scissors beats paper");
-        }else if (computerSelection === "paper")  {
+        } else if (computerSelection === "paper") {          
             return ("It's a tie! Play again");
         }
-    } 
+    }
 }
 
-function game(playerSelection, computerSelection) {
-
-    for(let i = 1; i < 6; i++) {
+function game() {
+    let n = 6;
+    let round;
+    for (let i = 1; i < n; i++) {
         playerSelection = (prompt("Choose an option: rock, paper or scissors")).toLowerCase();
         computerSelection = getComputerChoice();
         console.log(playerSelection);
         console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        round = playRound();
+        if(round === "It's a tie! Play again") {
+            n++;
+        }
+        console.log(round);
+    }
+    console.log(`Final score: ${score}`);
+
+    if (score >= 3){
+        console.log("You won the game!");
+    }else{
+        console.log("You lost the game!");
     }
 }
 
-console.log(game(playerSelection, computerSelection));
+game();
